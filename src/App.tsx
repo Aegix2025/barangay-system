aa9f618800a2cbceb4ac4987ce9b6a553f184994
AEGIX <edrian.clavel.delacruz@apcas.ph.education>
2026-08-12 23:47:14 +0800

    Fix duplicate API_URL declaration

--- src/App.tsx
+++ src/App.tsx
@@ -38,9 +38,6 @@ import {
   CivilStatus
 } from './types';
 
-// PALITAN ITO:
-const API_URL = 'http://localhost:5000/api/users';
-
 // NG GANITO (gamitin ang Vercel URL mo):
 const API_URL = 'https://barangay-system.vercel.app/api/users';
 
